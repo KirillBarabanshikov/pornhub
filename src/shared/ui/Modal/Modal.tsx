9e29ec272efa9603ecfe62b2f8187659a2b370fc
Kirill <kirillbar24@gmail.com>
2024-08-21 11:00:09 +0300

    added ui modal

---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,34 @@
+import { FC, PropsWithChildren } from 'react';
+import { createPortal } from 'react-dom';
+import { AnimatePresence, motion } from 'framer-motion';
+import clsx from 'clsx';
+import styles from './Modal.module.scss';
+
+interface IModal extends PropsWithChildren {
+    isOpen: boolean;
+    onClose: () => void;
+    title?: string;
+    className?: string;
+}
+
+export const Modal: FC<IModal> = ({ children, isOpen, onClose, title, className }) => {
+    return createPortal(
+        <AnimatePresence>
+            {isOpen && (
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    className={styles.overlay}
+                    onClick={onClose}
+                >
+                    <div className={clsx(styles.modal, className)} onClick={(e) => e.stopPropagation()}>
+                        {title && <h2 className={styles.title}>{title}</h2>}
+                        {children}
+                    </div>
+                </motion.div>
+            )}
+        </AnimatePresence>,
+        document.getElementById('portal') as HTMLDivElement,
+    );
+};
