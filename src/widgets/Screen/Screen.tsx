27b231f3caaea36e9f5a653aef1347d1684273bb
Kirill <kirillbar24@gmail.com>
2024-08-22 00:27:45 +0400

    screen v1

--- src/widgets/Screen/Screen.tsx
+++ src/widgets/Screen/Screen.tsx
@@ -1,5 +1,46 @@
 import styles from './Screen.module.scss';
+import 'react-resizable/css/styles.css';
+import { useEffect, useRef, useState } from 'react';
+import { motion } from 'framer-motion';
 
 export const Screen = () => {
-    return <div className={styles.screen} />;
+    const constraintsRef = useRef<HTMLDivElement>(null);
+
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+
+    useEffect(() => {
+        const savedPosition = localStorage.getItem('blockPosition');
+        if (savedPosition) {
+            setPosition(JSON.parse(savedPosition));
+        }
+    }, []);
+
+    const handleDrag = (e, info) => {
+        if (!constraintsRef.current) return;
+
+        const rectScreen = constraintsRef.current.getBoundingClientRect();
+        const rectTarget = e.target.getBoundingClientRect();
+
+        console.log();
+
+        const newPosition = { x: rectTarget.top - rectScreen.top, y: rectTarget.left - rectScreen.left };
+        setPosition(newPosition);
+        localStorage.setItem('blockPosition', JSON.stringify(newPosition));
+    };
+
+    return (
+        <div>
+            <motion.div ref={constraintsRef} className={styles.screen}>
+                <motion.div
+                    drag
+                    dragConstraints={constraintsRef}
+                    dragMomentum={false}
+                    dragElastic={0}
+                    dragTransition={{}}
+                    className={styles.resource}
+                    onDrag={(e, info) => console.log(info)}
+                />
+            </motion.div>
+        </div>
+    );
 };
