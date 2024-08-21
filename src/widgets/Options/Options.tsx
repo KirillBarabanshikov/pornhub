3710158f2a4bb449ecc477474e8d908c159b2a75
Kirill <kirillbar24@gmail.com>
2024-08-21 11:00:41 +0300

    fix options

--- src/widgets/Options/Options.tsx
+++ src/widgets/Options/Options.tsx
@@ -1,11 +1,51 @@
+import { useState } from 'react';
+import { IconButton, Modal } from '@/shared/ui';
+import PlusIcon from '@/shared/assets/icons/plus-lg.svg?react';
+import TrashIcon from '@/shared/assets/icons/trash-fill.svg?react';
 import styles from './Options.module.scss';
 
 export const Options = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <div className={styles.options}>
-            <div className={styles.option}></div>
-            <div className={styles.option}></div>
-            <div className={styles.option}></div>
+            <div className={styles.option}>
+                <div className={styles.title}>Источник аудио</div>
+                <div className={styles.sources}></div>
+                <div className={styles.actions}>
+                    <IconButton onClick={() => setIsOpen(true)}>
+                        <PlusIcon />
+                    </IconButton>
+                    <IconButton>
+                        <TrashIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className={styles.option}>
+                <div className={styles.title}>Источник видео</div>
+                <div className={styles.sources}></div>
+                <div className={styles.actions}>
+                    <IconButton>
+                        <PlusIcon />
+                    </IconButton>
+                    <IconButton>
+                        <TrashIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className={styles.option}>
+                <div className={styles.title}>Python scripts</div>
+                <div className={styles.sources}></div>
+                <div className={styles.actions}>
+                    <IconButton>
+                        <PlusIcon />
+                    </IconButton>
+                    <IconButton>
+                        <TrashIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} title={'Редактирование скрипта'} />
         </div>
     );
 };
